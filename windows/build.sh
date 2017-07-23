954e028ab8a16fe8e1072861d86ee597f13c876a
Roy Shmeuli <roysh9@gmail.com>
2017-07-23 22:37:27 +0300

    Adding codefresh ci workflow definition

--- windows/build.sh
+++ windows/build.sh
@@ -7,10 +7,6 @@ cd "$(dirname $( cd "$( dirname "$0" )" && pwd ) )"
 docker build \
     -t windows-installer \
     -f windows/Dockerfile \
-    --build-arg DOCKER_VERSION="17.06.0-ce-rc2" \
-    --build-arg VBOX_VERSION="5.1.22" \
-    --build-arg VBOX_REV="115126" \
-    --build-arg NWJS_VERSION="0.23.3" \
     .
 
 CONTAINER="$(docker run -d windows-installer)"
