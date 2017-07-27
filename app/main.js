2449079a32d8b263e069e78b36fd6c4e45680e89
Roy Shmeuli <roysh9@gmail.com>
2017-07-27 19:30:28 +0300

    Adding eslint to the project

--- app/main.js
+++ app/main.js
@@ -1,34 +1,36 @@
 'use strict'
 
-const { app, Menu, Tray, nativeImage } = require('electron')
+const { app, Menu, Tray } = require('electron')
 const path = require('path')
 const opn = require('opn')
-const { Server } = require('./server');
+const { Server } = require('./server')
 
-
-function getIcon() {
-    return path.join(__dirname, 'images', 'icon.png');
+function getIcon () {
+  return path.join(__dirname, 'images', 'icon.png')
 }
 
 let server = null
 let tray = null
 app.on('ready', () => {
+  server = new Server()
+  server.start()
 
-    server = new Server()
-    server.start()
-
-    tray = new Tray(getIcon())
-    const contextMenu = Menu.buildFromTemplate([
-        { label: 'Scoring', type: 'normal', click: () => {
-            opn('http://localhost:1390')
-        } },
-        { type: 'separator'},
-        { label: 'Quit', type: 'normal', role: 'quit' }
-    ])
-    tray.setToolTip('This is my application.')
-    tray.setContextMenu(contextMenu)
+  tray = new Tray(getIcon())
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Scoring',
+      type: 'normal',
+      click: () => {
+        opn('http://localhost:1390')
+      }
+    },
+    { type: 'separator' },
+    { label: 'Quit', type: 'normal', role: 'quit' }
+  ])
+  tray.setToolTip('This is my application.')
+  tray.setContextMenu(contextMenu)
 })
 
 app.on('before-quit', () => {
-    server.close()
-});
+  server.close()
+})
