b2e5ae64263e9dd2d8937c70e628a50dfac497d7
ThinkRedstone <ittai11@gmail.com>
2017-09-17 22:53:37 +0300

    added port number to modules in menu

--- app/menu.js
+++ app/menu.js
@@ -10,7 +10,7 @@ exports.buildAppMenu = modulesPromise => {
     .reduce((menuItems, module) => {
       if (module.link) {
         return menuItems.concat({
-          label: module.name,
+          label: module.name + ` | ${module.link.split(':')[2].replace('/', '')}`,
           type: 'normal',
           click: () => {
             opn(module.link)
