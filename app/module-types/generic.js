347f1e17cb55b089f737c29ae431a2f10ce89ce4
Roy Shmeuli <roysh9@gmail.com>
2018-04-08 16:01:48 +0300

    Adding support in generic modules

---
+++ app/module-types/generic.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const os = require('os')
+const Promise = require('bluebird')
+const { exec } = require('child_process')
+
+exports.GenericModule = class {
+  constructor (modulePath, description) {
+    this.name = description.name
+    this.path = modulePath
+
+    if (typeof description.command === 'string') {
+      this.command = description.command
+    } else {
+      this.command = description.command[os.platform()]
+    }
+
+    this.arguments = description.arguments || []
+
+    Object.freeze(this)
+  }
+
+  start (options) {
+    const stream = options.logStream
+    const child = exec(this.command, this.arguments, {
+      stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+      env: {
+        'DATA_DIR': options.datadir,
+        'AUTH_SECRET': options.secret,
+        'LOG_LEVEL': options.logLevel
+      }
+    })
+    child.stdout.pipe(stream)
+    child.stderr.pipe(stream)
+
+    return () => new Promise(resolve => {
+      child
+        .on('exit', resolve)
+        .kill()
+    })
+  }
+}
