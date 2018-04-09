6dd481a044f40499eb554250fa04736b3d2c54eb
Roy Shmeuli <roysh9@gmail.com>
2018-04-09 10:36:26 +0300

    remove old way to download modules

--- app/module-types/node.js
+++ app/module-types/node.js
@@ -1,18 +1,11 @@
 'use strict'
 
-const Promise = require('bluebird')
-const request = require('request')
+const fs = require('fs')
 const path = require('path')
-const tar = require('tar-fs')
-const { exec, fork, spawn } = require('child_process')
-const { createGunzip } = require('zlib')
 const rimraf = require('rimraf')
 const rotate = require('rotating-file-stream')
-const fs = require('fs')
-
-const requestAsync = Promise.promisify(request, { multiArgs: true })
-
-const NPM_REGISTRY_BASE_URL = 'https://registry.npmjs.org'
+const Promise = require('bluebird')
+const { fork, spawn } = require('child_process')
 
 exports.NodeModule = class {
   constructor (modulePath, description) {
@@ -72,66 +65,5 @@ exports.NodeModule = class {
       scriptPromise = Promise.resolve()
     }
     return Promise.all([scriptPromise, unlinkPromise])
-  }
-
-  download () {
-    return requestAsync({
-      baseUrl: NPM_REGISTRY_BASE_URL,
-      url: `/${this.package.replace('/', '%2F')}/`,
-      json: true
-    })
-      .then(([response, body]) => {
-        if (response.statusCode !== 200) {
-          throw new Error(`Module "${this.name}" not available`)
-        }
-
-        return body
-      })
-      .then(body => {
-        const { tarball } = body.versions[this.version].dist
-
-        return new Promise((resolve, reject) => {
-          const req = request({
-            url: tarball,
-            encoding: null,
-            gzip: true
-          })
-
-          req.on('response', response => {
-            if (response.statusCode === 200) {
-              req
-                .pipe(createGunzip())
-                .pipe(tar.extract(this.path, {
-                  map: header => {
-                    if (header.name.startsWith('package/')) {
-                      header.name = header.name.substring('package/'.length)
-                    }
-                    return header
-                  }
-                })
-                  .on('finish', () => {
-                    resolve()
-                  }))
-                .on('error', reject)
-            } else {
-              reject(new Error(`Could not download module: ${this.name}`))
-            }
-          })
-        })
-      })
-      .then(() => new Promise((resolve, reject) => {
-        exec('yarn --production --pure-lockfile', {
-          cwd: this.path,
-          stdio: 'ignore'
-        })
-          .on('error', reject)
-          .on('close', code => {
-            if (code === 0) {
-              resolve()
-            } else {
-              reject(new Error(`Could not install module: ${this.name}`))
-            }
-          })
-      }))
   }
 }
