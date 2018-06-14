561ae0471bf1a37b961184d59ad989a58f56c763
Idan Stark <idanstark42@gmail.com>
2018-06-14 19:28:32 +0300

    Added password field

---
+++ src/renderer/components/settings/field/password.vue
@@ -0,0 +1,33 @@
+<template>
+    {{ field.display || field.name }}
+    <button @click="editing = !editing"></button>
+    <div v-if="editing">
+        <label>
+            <input
+                type="password"
+                :name="field.name"
+                :minLength="field.minLength"
+                :maxLength="field.maxLength"
+                :value="value"
+            />
+        </label>
+        <button @click="$emit('value-change', sha256(value))"></button>
+    </div>
+</template>
+
+<script>
+
+  import sha256 from 'sha256'
+  
+  export default {
+    name: 'password-field',
+    props: ['field'],
+    editing: false,
+    value: '',
+    sha256
+  }
+</script>
+
+<style scoped>
+
+</style>
