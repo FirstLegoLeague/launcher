86dd3da5604b15c4b0c9ad65588939988aaea246
Roy Shmeuli <roysh9@gmail.com>
2018-07-30 23:13:56 +0300

    Adding modules sites to home page

--- src/main/adapters/home.js
+++ src/main/adapters/home.js
@@ -1,7 +1,28 @@
 'use strict'
 
+const opn = require('opn')
+const Promise = require('bluebird')
+
 const { saveLogs } = require('../logs')
 
-exports.saveLogs = function (filename, callback) {
-  saveLogs(filename).asCallback(callback)
+exports.HomeAdapter = class {
+  constructor (server) {
+    this.server = server
+  }
+
+  getPortsAllocation (callback) {
+    this.server.getPortsAllocation().asCallback(callback)
+  }
+
+  getIp (callback) {
+    this.server.getIp().asCallback(callback)
+  }
+
+  saveLogs (filename, callback) {
+    saveLogs(filename).asCallback(callback)
+  }
+
+  openSite (site, callback) {
+    Promise.resolve(opn(site)).asCallback(callback)
+  }
 }
