f451bfc6a42e6afb37be2bb078f47ce8e7f8dedf
2roy999 <roysh9@gmail.com>
2018-08-10 16:14:32 +0300

    Upgrade clock to version 2.0.4

--- dev-scripts/config.get.js
+++ dev-scripts/config.get.js
@@ -61,7 +61,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/clock',
-        version: '2.0.2'
+        version: '2.0.4'
       }
     }
   },
