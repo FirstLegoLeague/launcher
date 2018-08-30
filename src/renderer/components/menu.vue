c586da8add412f1dde7428dac6c2467948c5d21e
Idan Stark <idanstark42@gmail.com>
2018-08-31 02:18:17 +0300

    Added icons to menu

--- src/renderer/components/menu.vue
+++ src/renderer/components/menu.vue
@@ -2,9 +2,15 @@
     <div class="top-bar">
         <div class="top-bar-left top-par-page">
             <ul class="menu">
-              <router-link tag="li" active-class="is-active" :to="{ name: 'home-page' }"><a>Home</a></router-link>
-              <router-link tag="li" active-class="is-active" :to="{ name: 'settings' }"><a>Settings</a></router-link>
-              <router-link tag="li" active-class="is-active" :to="{ name: 'about-page' }"><a>About</a></router-link>
+              <router-link tag="li" active-class="is-active" :to="{ name: 'home-page' }">
+                <a><i class="fas fa-home"></i>&nbsp;Home</a>
+              </router-link>
+              <router-link tag="li" active-class="is-active" :to="{ name: 'settings' }">
+                <a><i class="fas fa-cogs"></i>&nbsp;Settings</a>
+              </router-link>
+              <router-link tag="li" active-class="is-active" :to="{ name: 'about-page' }">
+                <a><i class="fas fa-info-circle"></i>&nbsp;About</a>
+              </router-link>
             </ul>
         </div>
     </div>
