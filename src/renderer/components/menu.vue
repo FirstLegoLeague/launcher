d27c05bf7a28b5923dbbc9a12787233bca9aeb93
Idan Stark <idanstark42@gmail.com>
2018-09-14 00:56:50 +0300

    Added link to help from top right menu

--- src/renderer/components/menu.vue
+++ src/renderer/components/menu.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="top-bar">
-        <div class="top-bar-left top-par-page">
+        <div class="top-bar-left">
             <ul class="menu">
               <router-link tag="li" active-class="is-active" :to="{ name: 'home-page' }">
                 <a><i class="fas fa-home"></i>&nbsp;Home</a>
@@ -13,12 +13,33 @@
               </router-link>
             </ul>
         </div>
+        <div class="top-bar-right">
+          <ul class="menu">
+            <li><a @click="event => openSite(event, this.helpLink)"><i class="fas fa-question-circle"></i>&nbsp;Help</a></li>
+          </ul>
+        </div>
     </div>
 </template>
 
 <script>
+  import Promise from 'bluebird'
+
   export default {
-    name: 'main-menu'
+    name: 'main-menu',
+    methods: {
+      openSite (event, site) {
+        event.preventDefault()
+
+        Promise.fromCallback(cb => this.adapter.openSite(site, cb))
+          .catch(err => {
+            console.error(err)
+          })
+      }
+    },
+    mounted () {
+      this.adapter = this.$electron.remote.require('./main').homeAdapter
+      this.helpLink = 'https://github.com/FirstLegoLeague/Launcher/blob/user-guide/docs/userguide.md#first-lego-league-tms-user-guide'
+    }
   }
 </script>
 
