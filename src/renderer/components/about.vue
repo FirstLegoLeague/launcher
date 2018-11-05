4128468832b74e7ecacb017a82c59753fce32bf3
Idan Stark <idanstark42@gmail.com>
2018-11-05 20:58:15 +0200

    Using pre

--- src/renderer/components/about.vue
+++ src/renderer/components/about.vue
@@ -12,8 +12,9 @@ or you have any suggestions or questions, please contact us:
                     <div class="cell small-4">Email FLL-TMS-Support@firstinspires.org</div>
                     <div class="cell small-4"><a @click="event => openSite(event, 'http://fll-tools.com/support/')">Visit our website Support Page</a></div>
                 </div>
-                <div>When reporting a bug, please click "Save debug data" to export the software logs and the team scores database. Attach the zip file to the email or support ticket.
-</div>
+                <pre>When reporting a bug, please click "Save debug data" to export the software logs and the team scores database.
+Attach the zip file to the email or support ticket.
+</pre>
                 <a class="button" @click="saveDebugData">Save debug data</a>
                 <h4>Credits</h4>
                 <pre>Many people took part in the development of this software. We appreciate the help of:
