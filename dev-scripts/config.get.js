56113bb61bfb1972dd430374c7812b97ef5de8d6
Alan Green <alanggreen@gmail.com>
2018-11-07 00:09:47 +0200

    Update display 2.2.5 and scoring 2.1.8

--- dev-scripts/config.get.js
+++ dev-scripts/config.get.js
@@ -35,7 +35,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/display',
-        version: '2.2.4'
+        version: '2.2.5'
       }
     },
     tournament: {
@@ -49,7 +49,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/scoring',
-        version: '2.1.7'
+        version: '2.1.8'
       }
     },
     rankings: {
