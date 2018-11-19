66e68db4ecdf9ba54dc60b1883f1b0d17aee6df3
Idan Stark <idanstark42@gmail.com>
2018-11-19 23:47:25 +0200

    logging the network interfaces after and before cleansing

--- src/main/network.js
+++ src/main/network.js
@@ -1,11 +1,16 @@
 
 const os = require('os')
 
+const { logger } = require('./logs')
+
 const networkInterfaces = Object.entries(os.networkInterfaces())
   .map(([name, interfaces]) => [name, interfaces.filter(i => i.family === 'IPv4')])
   .filter(([name, interfaces]) => interfaces.length)
   .map(([name, interfaces]) => ({ [name]: interfaces }))
   .reduce((obj, entry) => Object.assign(obj, entry), {})
+
+logger.debug(os.networkInterfaces())
+logger.debug(networkInterfaces)
 
 exports.networkInterfaces = () => networkInterfaces
 
