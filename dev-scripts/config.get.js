a6e918575fa0b9b6ed91cb2a8858543ff108c7b5
Alan Green <alanggreen@gmail.com>
2018-11-27 09:03:54 +0200

    Update scoring to 2.2.4

--- dev-scripts/config.get.js
+++ dev-scripts/config.get.js
@@ -49,7 +49,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/scoring',
-        version: '2.2.3'
+        version: '2.2.4'
       }
     },
     rankings: {
