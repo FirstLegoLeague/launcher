fa6b0e7a7670733add2ea498801fed7e382871e1
Tal Taub <taltaub22@gmail.com>
2019-01-14 11:49:07 +0200

    v2018.2.0 with old scoring

--- dev-scripts/config.get.js
+++ dev-scripts/config.get.js
@@ -56,7 +56,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/scoring',
-        version: '2.3.1'
+        version: '2.2.4'
       }
     },
     rankings: {
