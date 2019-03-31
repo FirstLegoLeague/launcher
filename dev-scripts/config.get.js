3888247f2d44339ed5b15f19ed9e2ae0c45add58
Alan Green <alanggreen@gmail.com>
2019-03-31 21:58:36 +0300

    Update display 2.4.7

--- dev-scripts/config.get.js
+++ dev-scripts/config.get.js
@@ -35,7 +35,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/display',
-        version: '2.4.6'
+        version: '2.4.7'
       }
     },
     tournament: {
