3c9b756a1ddc656ffd15413d10ea40d0d708a7a2
Ofek Ashery <ofek.ashery@gmail.com>
2019-08-10 19:34:35 +0300

    Revert "Done with the macOS run script"
    This reverts commit 1fb9f05612d63844e7c48b5030a14c98818c88ba.

--- macos/afterPackHook.js
+++ macos/afterPackHook.js
@@ -1,16 +1,3 @@
-
-const fs = require('fs')
-const path = require('path')
-const { exec } = require('child_process')
-
 exports.default = async function (context) {
-  if (context.appOutDir.endsWith('/mac')) {
-    fs.copyFile(path.resolve(__dirname, './setup/run'), context.appOutDir + '/run', (err) => {
-      if (err) throw err
-      console.log('The run script was copied to dist/mac.\nCreating the mac.zip...')
-      exec('cd ./dist && zip -r -X mac.zip mac', (err) => {
-        if (err) throw err
-      })
-    })
-  }
+  console.log(context)
 }
