79d44f442dbe3a8cec7dfd2b856ade71b2137574
Alan Green <alanggreen@gmail.com>
2019-09-23 21:18:42 +0300

    Spanish scoresheet for es-UY

--- dev-scripts/config.get.js
+++ dev-scripts/config.get.js
@@ -57,7 +57,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/scoring',
-        version: '2.4.12'
+        version: '2.4.13'
       }
     },
     tournament: {
