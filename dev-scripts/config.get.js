c9d608a0ab76d958cd3898dc1d27f9cde672b679
Alan Green <alanggreen@gmail.com>
2019-09-25 21:41:43 +0300

    scorning

--- dev-scripts/config.get.js
+++ dev-scripts/config.get.js
@@ -57,7 +57,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/scoring',
-        version: '2.4.13'
+        version: '2.4.14'
       }
     },
     tournament: {
