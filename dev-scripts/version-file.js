2265d380bff1ebddcd11bd0622fb40a6c4a7cf16
Roy Shmueli <2903801+2roy999@users.noreply.github.com>
2019-10-03 14:44:17 +0300

    fix

--- dev-scripts/version-file.js
+++ dev-scripts/version-file.js
@@ -1,11 +1,14 @@
 
 const caporal = require('caporal')
+const fs = require('fs')
+const path = require('path')
 
 caporal
   .name('version-file')
   .version('v1')
   .option('--commit, -c <commit>', 'The git commit of the version')
   .option('--ts <timestamp>', 'The timestamp of the build')
+  .option('--output <output>', 'The output file')
   .argument('<semver>', 'The semver version for the file')
   .action((args, options) => {
     const timestamp = (options.ts) ? Number(options.ts) : Date.now()
@@ -15,7 +18,7 @@ caporal
       return
     }
 
-    console.log(JSON.stringify({
+    fs.writeFileSync(path.resolve(options.output), JSON.stringify({
       semver: args.semver || 'none',
       timestamp: timestamp,
       commit: options.commit || 'NA'
