e5e2aa32294c1209b6260f0b106e0504cc409b2c
Alan Green <alanggreen@gmail.com>
2019-10-12 17:22:13 +0300

    Test fr_CA scoresheet

--- dev-scripts/config.get.js
+++ dev-scripts/config.get.js
@@ -57,7 +57,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/scoring',
-        version: '2.4.17'
+        version: '2.4.18'
       }
     },
     tournament: {
