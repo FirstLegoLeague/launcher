08a1863688a77bc8f094ec9ac9983a93ff1eaf0e
Alan Green <alanggreen@gmail.com>
2020-02-09 17:42:15 +0200

    Arabic scoresheet RTL

--- dev-scripts/config.get.js
+++ dev-scripts/config.get.js
@@ -57,7 +57,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/scoring',
-        version: '2.4.31-test'
+        version: '2.4.31-test2'
       }
     },
     tournament: {
