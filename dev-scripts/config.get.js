1f450ac4ec7e204b233d960eba4961974f97a9e4
Alan Green <alanggreen@gmail.com>
2020-03-07 06:55:09 +0200

    Update clock to 2.6.10 big buttons

--- dev-scripts/config.get.js
+++ dev-scripts/config.get.js
@@ -16,7 +16,7 @@ module.exports = {
       get: getNpmModule,
       options: {
         package: '@first-lego-league/clock',
-        version: '2.6.8'
+        version: '2.6.10'
       }
     },
     display: {
