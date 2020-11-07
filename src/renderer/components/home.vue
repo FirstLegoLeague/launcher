887f3d981ed94564c092565c8314881dc40d5c34
AlanGreen <alang@ctera.com>
2020-11-07 09:39:18 +0200

    Update to 2020.1.1

--- src/renderer/components/home.vue
+++ src/renderer/components/home.vue
@@ -7,7 +7,7 @@
           <div class="ten wide column">
             <h2 class="ui header">
               Tournament Management System
-              <span class="sub header">v2020.1.0</span>
+              <span class="sub header">v2020.1.1</span>
             </h2>
           </div>
           <div id="fll" class="three wide column"></div>
